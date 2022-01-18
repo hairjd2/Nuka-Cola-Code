22d453712891b49d3bf5f597d97b972bfbe59eb3
hairjd2 <hairjd2@gmail.com>
2022-01-18 16:56:22 -0500

    Got motor working
    A 5V DC motor now works with the circuit. Speed and time will need to be changed.

--- Nuka-Cola-Code.ino
+++ Nuka-Cola-Code.ino
@@ -1,43 +1,41 @@
-const int quarterButton = 2; //quarter button
-const int dimeButton = 4;
-const int ledPin = 13;// the number of the LED pin
-const int neededValue = 75;
+#include <Stepper.h>
+
+const int CAP_BUTTON = 2; //quarter button
+const int LED_PIN = 13;// the number of the LED pin
+const int NEEDED_VALUE = 4;
+const int MOTOR_SPEED = 50;
+const int MOTOR_PIN = 9;
+const int LOW_SPEED = 0;
+const int HIGH_SPEED = 100;
 
 // Variables will change:
 int ledState = LOW;
-int quarterState;
-int dimeState;
-int lastQuarterState = LOW;
-int lastDimeState = LOW;
+int capState;
+int lastCapState = LOW;
 unsigned long lastDebounceTime = 0;
 unsigned long debounceDelay = 50;
 int inputtedValue = 0;
 
 void setup() {
-  pinMode(quarterButton, INPUT);
-  pinMode(dimeButton, INPUT);
-  pinMode(ledPin, OUTPUT);
-
+  pinMode(CAP_BUTTON, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(MOTOR_PIN, OUTPUT);
   // set initial LED state
-  digitalWrite(ledPin, ledState);
+  digitalWrite(LED_PIN, ledState);
+  // digitalWrite(MOTOR_PIN, LOW);
+  analogWrite(MOTOR_PIN, 0);
 }
 
 void loop() {
   // read the state of the switch into a local variable:
-  int readingQuarter = digitalRead(quarterButton);
-  int readingDime = digitalRead(dimeButton);
+  int readingCap = digitalRead(CAP_BUTTON);
 
   // check to see if you just pressed the button
   // (i.e. the input went from LOW to HIGH), and you've waited long enough
   // since the last press to ignore any noise:
 
   // If the switch changed, due to noise or pressing:
-  if (readingQuarter != lastQuarterState) {
-    // reset the debouncing timer
-    lastDebounceTime = millis();
-  }
-
-  if (readingDime != lastDimeState) {
+  if (readingCap != lastCapState) {
     // reset the debouncing timer
     lastDebounceTime = millis();
   }
@@ -47,37 +45,30 @@ void loop() {
     // delay, so take it as the actual current state:
 
     // if the button state has changed:
-    if (readingQuarter != quarterState) {
-      quarterState = readingQuarter;
+    if (readingCap != capState) {
+      capState = readingCap;
 
 
       // only toggle the LED if the new button state is HIGH
-      if (quarterState == HIGH) {
-        inputtedValue += 25;
+      if (capState == HIGH) {
+        inputtedValue++;
       }
     }
 
-    if (readingDime != dimeState) {
-      dimeState = readingDime;
-
-
-      // only toggle the LED if the new button state is HIGH
-      if (dimeState == HIGH) {
-        inputtedValue += 10;
-      }
-    }
-
-    if(inputtedValue >= neededValue) {
-      inputtedValue -= neededValue;
-      digitalWrite(ledPin, HIGH);
+    if(inputtedValue >= NEEDED_VALUE) {
+      inputtedValue -= NEEDED_VALUE;
+      digitalWrite(LED_PIN, HIGH);
+      // digitalWrite(MOTOR_PIN, HIGH);
+      analogWrite(MOTOR_PIN, 100);
       delay(5000);
+      analogWrite(MOTOR_PIN, 0);
+      // digitalWrite(MOTOR_PIN, LOW);
     }
   }
 
   // set the LED:
-  digitalWrite(ledPin, LOW);
+  digitalWrite(LED_PIN, LOW);
 
   // save the reading. Next time through the loop, it'll be the lastButtonState:
-  lastQuarterState = readingQuarter;
-  lastDimeState = readingDime;
+  lastCapState = readingCap;
 }
